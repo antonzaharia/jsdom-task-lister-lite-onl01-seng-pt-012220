93d69da1489ab04c9622e855e976410efe4d8131
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-07-21 21:56:02 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
     let li = document.createElement('li');
     let liText = document.createTextNode(input);
     let imp = document.getElementById("new-task-imp").value;
-    if (imp == "first"){
+    if (imp === "first"){
       // liText.style.color = "red";
       console.log(imp);
     }
